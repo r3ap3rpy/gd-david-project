f014bf566956d4802e0ffc7b5efe41c1fc390b49
Szabó Dániel Ernő <r3ap3rpy@gmail.com>
2025-12-01 10:26:01 +0100

    Added new context route to feature branch

--- flask_app/app.py
+++ flask_app/app.py
@@ -10,5 +10,9 @@ def index():
 def about():
     return "This is the about page!"
 
+@app.route("/feature")
+def feature():
+    return "This is for the demo of the branch!"
+
 if __name__ == "__main__":
     app.run("0.0.0.0", port = 8080, debug = True)
